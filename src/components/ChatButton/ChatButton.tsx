43f40fd28c305d1ec210129ece1c661ea5de153a
Andreas Tröbacher <treobacher.andreas@yahoo.de>
2024-11-26 16:38:11 +0100

    ✨ Add additional setup for components library project ✨ Add first component to display chat button

---
+++ src/components/ChatButton/ChatButton.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import {IChatButton} from "./interfaces/ChatButton.model";
+
+const ChatButtonComponent = (props: IChatButton) => {
+    return <>
+        <button></button>
+    </>;
+}
+
+export default ChatButtonComponent;
---
+++ src/components/index.ts
@@ -0,0 +1,1 @@
+export {default as ChatButtonComponent} from "./ChatButton";
