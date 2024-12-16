f32535612679b605f87ae68fa32f43c6b6195b3e
Andreas Tröbacher <treobacher.andreas@yahoo.de>
2024-12-16 18:16:47 +0100

    ✨ Add service for text to speech implementation 🔥 Remove api key from file and add to local file

--- src/components/ChatButton/ChatButton.tsx
+++ src/components/ChatButton/ChatButton.tsx
@@ -4,51 +4,19 @@ import {IChatButton} from "./interfaces/ChatButton.model";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faRobot} from "@fortawesome/free-solid-svg-icons";
 import './ChatButton.css';
-import OpenAI from "openai";
+import TextToSpeechService from "../../shared/TextToSpeech.service";
 
 const ChatButtonComponent = (props: IChatButton) => {
 
-    const audioContextRef = useRef<AudioContext | null>(null);
-    const sourceNodeRef = useRef<MediaStreamAudioSourceNode | null>(null);
     const [audioElement, setAudioElement] = useState<HTMLAudioElement>();
     const [didUserInteract, setDidUserInteract] = useState<boolean>(false);
     const [didPlayInstructions, setDidPlayInstructions] = useState<boolean>(false);
 
     useEffect(() => {
-        const handleAudioStream = async (stream: ReadableStream<Uint8Array>) => {
-            const audioElement = new Audio();
-            audioElement.src = URL.createObjectURL(new Blob([await streamToArrayBuffer(stream)], {type: "audio/mpeg"}));
+        const tts = new TextToSpeechService();
+        tts.getSpeechByText(props.accessibilityText).then(audioElement => {
             setAudioElement(audioElement);
-        };
-
-        const streamToArrayBuffer = async (stream: ReadableStream<Uint8Array>) => {
-            const chunks: Uint8Array[] = [];
-            const reader = stream.getReader();
-            let result;
-            while (!(result = await reader.read()).done) {
-                chunks.push(result.value);
-            }
-            return new Blob(chunks).arrayBuffer();
-        };
-
-        const tts = async () => {
-            const openai = new OpenAI({
-                apiKey: 'sk-svcacct-ECrbLVbnY4YCnjcq3X_KUZhpzv3wP86XB2eXhvNsSNb6_1LrvfcqWlrHaeZvSnAy58qgPRT3BlbkFJJc23BWnQVmz3Pn15rTbB5GiIEliEbGJ_oqwkGRDu-ZDPNWpwFx8nNGVSyCNkFFOgymOpwA',
-                dangerouslyAllowBrowser: true
-            });
-
-            const response = await openai.audio.speech.create({
-                model: "tts-1",
-                voice: "alloy",
-                input: props.accessibilityText,
-            });
-
-            if (response.body) {
-                const audioStream = response.body;
-                await handleAudioStream(audioStream);
-            }
-        }
-        tts();
+        });
     }, [props.accessibilityText]);
 
     useEffect(() => {
---
+++ src/shared/TextToSpeech.service.ts
@@ -0,0 +1,40 @@
+import OpenAI from "openai";
+import {environment} from "../../environment";
+
+class TextToSpeechService {
+    async handleAudioStream(stream: ReadableStream<Uint8Array>){
+        const audioElement = new Audio();
+        audioElement.src = URL.createObjectURL(new Blob([await this.streamToArrayBuffer(stream)], {type: "audio/mpeg"}));
+        return audioElement;
+    }
+
+    async streamToArrayBuffer(stream: ReadableStream<Uint8Array>){
+        const chunks: Uint8Array[] = [];
+        const reader = stream.getReader();
+        let result;
+        while (!(result = await reader.read()).done) {
+            chunks.push(result.value);
+        }
+        return new Blob(chunks).arrayBuffer();
+    }
+
+    async getSpeechByText(text: string){
+        const openai = new OpenAI({
+            apiKey: environment.apiKey,
+            dangerouslyAllowBrowser: true
+        });
+
+        const response = await openai.audio.speech.create({
+            model: "tts-1",
+            voice: "alloy",
+            input: text,
+        });
+
+        if (response.body) {
+            const audioStream = response.body;
+            return await this.handleAudioStream(audioStream);
+        }
+    }
+}
+
+export default TextToSpeechService;
